34cd109ac750d3541c7f439b53ada2e3485710a0
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-05-06 18:07:08 -0400

    freeGeoIp implemented

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIp } = require('./iss')
+const { fetchMyIp, fetchCoordsByIp } = require('./iss')
 
 fetchMyIp((err, data) => {
   if (err) {
@@ -6,4 +6,12 @@ fetchMyIp((err, data) => {
     return;
   }
   console.log("your IP is: ", data)
+})
+
+fetchCoordsByIp("76.69.79.165", (err, data)=> {
+  if (err) {
+    console.log("Failed to get Data: ", err)
+    return;
+  }
+  console.log("your cords are: ", data)
 })
--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const request = require("request");
 const ipifyAPI = 'https://api.ipify.org/?format=json';
+const freeGeoipAPI = 'https://freegeoip.app/json/';
 
 const fetchMyIp = function(callback) {
   request(ipifyAPI, (error, response, body) => {
@@ -16,4 +17,20 @@ const fetchMyIp = function(callback) {
   });
 };
 
-module.exports = { fetchMyIp };
+const fetchCoordsByIp = function(ip, callback) {
+  request(`${freeGeoipAPI}${ip}`, (error, response, body)=> {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = { fetchMyIp, fetchCoordsByIp };
